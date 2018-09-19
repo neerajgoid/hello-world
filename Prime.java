369cdbd3582dcc40932e1181629bf7cbfe3ff3dc
neerajgoid <43369991+neerajgoid@users.noreply.github.com>
2018-09-19 09:48:16 +0530

    Add files via upload

---
+++ Prime.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Prime {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Enter your number");
+		Scanner sc = new Scanner(System.in);
+		
+		int num = sc.nextInt();
+        int count = 0;
+        for(int i = 1; i <= num; i++)
+        {
+            // condition for nonprime number
+            if(num % i == 0)
+            {
+                count++;
+               
+            }
+        }
+        System.out.println("count is " + count);
+        if (count<=2)
+            System.out.println(num + " is a prime number.");
+        else
+            System.out.println(num + " is not a prime number.");
+
+	}
+
+}
