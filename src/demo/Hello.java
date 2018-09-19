cbbf073b0ccb2bbb3566ea6d137df8c360bf8f99
NEERAJ GOIDANI <neeraj.goidani@globant.com>
2018-09-19 11:18:32 +0530

    changed the hello world prog to a fibonnaci program

--- src/demo/Hello.java
+++ src/demo/Hello.java
@@ -1,11 +1,29 @@
 package demo;
 
+import java.util.Scanner;
+
 public class Hello {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello world");
 		System.out.println("Printing fibonnaci series");
+		int a = 0,b = 1;
+		
+		System.out.println("Enter limit");
+		Scanner sc = new Scanner(System.in);
+		int limit = sc.nextInt();
+		int c=a+b;
+		System.out.print(a + " " + b + " ");
+		while(c<limit)
+		{
+			
+			System.out.print(c + " ");
+			a=b;
+			b=c;
+			c=a+b;
+		}
+		
 		
 
 	}
