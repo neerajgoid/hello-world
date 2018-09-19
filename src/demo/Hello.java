11f815727cf2dff78c0eeaeb77e69f825a835c33
NEERAJ GOIDANI <neeraj.goidani@globant.com>
2018-09-19 11:05:51 +0530

    New program- Hello World added

---
+++ src/demo/Hello.java
@@ -0,0 +1,13 @@
+package demo;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello world");
+		System.out.println("Printing fibonnaci series");
+		
+
+	}
+
+}
